1d530029c1dc7835a78734b104ffb6d70287addf
Keshav Aditya R.P <keshavaditya26896@gmail.com>
2017-10-15 15:09:55 +0530

    Existing Pages Completed, Images yet to be Added, Go to Top Added, Breadcrumb Style Navigation Added

--- js/javascript.js
+++ js/javascript.js
@@ -17,20 +17,20 @@ function consoleText(words, id, colors) {
   var letterCount = 1;
   var x = 1;
   var waiting = false;
-  var target = document.getElementById(id)
-  target.setAttribute('style', 'color:' + colors[0])
+  var target = document.getElementById(id);
+  target.setAttribute('style', 'color:' + colors[0]);
   window.setInterval(function() {
 
     if (letterCount === 0 && waiting === false) {
       waiting = true;
-      target.innerHTML = words[0].substring(0, letterCount)
+      target.innerHTML = words[0].substring(0, letterCount);
       window.setTimeout(function() {
         var usedColor = colors.shift();
         colors.push(usedColor);
         var usedWord = words.shift();
         words.push(usedWord);
         x = 1;
-        target.setAttribute('style', 'color:' + colors[0])
+        target.setAttribute('style', 'color:' + colors[0]);
         letterCount += x;
         waiting = false;
       }, 1000)
@@ -42,23 +42,22 @@ function consoleText(words, id, colors) {
         waiting = false;
       }, 1000)
     } else if (waiting === false) {
-      target.innerHTML = words[0].substring(0, letterCount)
+      target.innerHTML = words[0].substring(0, letterCount);
       letterCount += x;
     }
   }, 120)
   window.setInterval(function() {
     if (visible === true) {
-      con.className = 'console-underscore hidden'
+      con.className = 'console-underscore hidden';
       visible = false;
 
     } else {
-      con.className = 'console-underscore'
+      con.className = 'console-underscore';
 
       visible = true;
     }
   }, 400)
 }
-
 
 // //List of quotes
 // var quotes = ["RE", "REGU", "REGULO", "REGULOGE", "REGULOGEM"];
